17ab248bacfb8eb1683b366d2c1812c1ddc59214
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-11-25 08:47:10 +0000

    save

--- mean/src/app/app.component.ts
+++ mean/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mean';
+export class AppComponent implements OnInit{
+results !: any;
+// Inject HttpClient into your component or service.
+constructor(private http: HttpClient) {}
+ngOnInit(): void {
+// Make the HTTP request:
+this.http.get('https://3000-polisenoric-meanimparan-85uzy8u1qpw.ws-eu116.gitpod.io/api').subscribe(data => {
+// Read the result field from the JSON response.
+this.results = data;
+console.log(this.results)
+});
+}
 }
